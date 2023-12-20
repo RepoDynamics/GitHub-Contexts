c9a25995d787139d1ebdc9b26f545501accae72c
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2023-12-21 00:29:06 +0100

    Update push.py

--- src/github_contexts/github/payloads/push.py
+++ src/github_contexts/github/payloads/push.py
@@ -1,29 +1,32 @@
-class PushPayload(EventPayload):
+from github_contexts.github.payloads.base import Payload
+from github_contexts.github.payloads.objects.commit import CommitObject
+from github_contexts.github.payloads.objects.commit_author import CommitAuthorObject
+from github_contexts.github.enums import ActionType
+
+
+class PushPayload(Payload):
+
     def __init__(self, payload: dict):
         super().__init__(payload=payload)
-        self._payload = payload
         return
 
     @property
-    def action(
-        self,
-    ) -> Literal[
-        WorkflowTriggeringAction.CREATED, WorkflowTriggeringAction.DELETED, WorkflowTriggeringAction.EDITED
-    ]:
-        """Push action type."""
-        if self._payload["created"]:
-            return WorkflowTriggeringAction.CREATED
-        if self._payload["deleted"]:
-            return WorkflowTriggeringAction.DELETED
-        return WorkflowTriggeringAction.EDITED
+    def action(self) -> ActionType:
+        """Push action type; either 'created', 'deleted', or 'edited'."""
+        if self.created:
+            return ActionType.CREATED
+        if self.deleted:
+            return ActionType.DELETED
+        return ActionType.EDITED
 
     @property
-    def head_commit(self) -> dict:
-        return self._payload["head_commit"]
+    def after(self) -> str:
+        """The SHA hash of the most recent commit on the branch after the event."""
+        return self._payload["after"]
 
     @property
-    def head_commit_message(self) -> str:
-        return self.head_commit["message"]
+    def base_ref(self) -> str | None:
+        return self._payload.get("base_ref")
 
     @property
     def before(self) -> str:
@@ -31,7 +34,41 @@ class PushPayload(EventPayload):
         return self._payload["before"]
 
     @property
-    def after(self) -> str:
-        """The SHA hash of the most recent commit on the branch after the event."""
-        return self._payload["after"]
+    def commits(self) -> list[CommitObject]:
+        """List of pushed commits."""
+        return [CommitObject(commit) for commit in self._payload["commits"]]
 
+    @property
+    def compare(self) -> str:
+        """URL comparing the before and after commits."""
+        return self._payload["compare"]
+
+    @property
+    def created(self) -> bool:
+        """Whether the push created the reference."""
+        return self._payload["created"]
+
+    @property
+    def deleted(self) -> bool:
+        """Whether the push deleted the reference."""
+        return self._payload["deleted"]
+
+    @property
+    def forced(self) -> bool:
+        """Whether the push was forced."""
+        return self._payload["forced"]
+
+    @property
+    def head_commit(self) -> CommitObject | None:
+        """The most recent commit on the branch after the event."""
+        return CommitObject(self._payload["head_commit"]) if self._payload.get("head_commit") else None
+
+    @property
+    def pusher(self) -> CommitAuthorObject:
+        """The user that pushed the commits."""
+        return CommitAuthorObject(self._payload["pusher"])
+
+    @property
+    def ref(self) -> str:
+        """The full reference name that was pushed to, e.g.: 'refs/heads/main', 'refs/tags/v1.0.0'."""
+        return self._payload["ref"]
