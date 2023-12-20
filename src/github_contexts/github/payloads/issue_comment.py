b87a4b623b045f8643f861da94b3e8066084b286
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2023-12-21 00:28:28 +0100

    Update issue_comment.py

--- src/github_contexts/github/payloads/issue_comment.py
+++ src/github_contexts/github/payloads/issue_comment.py
@@ -1,57 +1,40 @@
+from github_contexts.github.payloads.base import Payload
+from github_contexts.github.enums import ActionType
+from github_contexts.github.payloads.objects.comment import CommentObject
+from github_contexts.github.payloads.objects.issue import IssueObject
+from github_contexts.github.payloads.objects.changes import IssueCommentEditedChangesObject
 
-class IssueCommentPayload(EventPayload):
+
+class IssueCommentPayload(Payload):
     def __init__(self, payload: dict):
         super().__init__(payload=payload)
         return
 
     @property
-    def action(self) -> Literal[
-        WorkflowTriggeringAction.CREATED, WorkflowTriggeringAction.EDITED, WorkflowTriggeringAction.DELETED
-    ]:
-        """Comment action type that triggered the event."""
-        return WorkflowTriggeringAction(self._payload["action"])
+    def action(self) -> ActionType:
+        """Action that triggered the event;
+        either 'created', 'edited', or 'deleted'.
+        """
+        return ActionType(self._payload["action"])
 
     @property
-    def comment(self) -> dict:
+    def comment(self) -> CommentObject:
         """Comment data."""
-        return self._payload["comment"]
+        return CommentObject(self._payload["comment"])
 
     @property
-    def issue(self) -> dict:
+    def issue(self) -> IssueObject:
         """Issue data."""
-        return self._payload["issue"]
+        return IssueObject(self._payload["issue"])
 
     @property
-    def author_association(
-        self,
-    ) -> Literal[
-        "COLLABORATOR",
-        "CONTRIBUTOR",
-        "FIRST_TIMER",
-        "FIRST_TIME_CONTRIBUTOR",
-        "MANNEQUIN",
-        "MEMBER",
-        "NONE",
-        "OWNER",
-    ]:
-        return self.comment["author_association"]
-
-    @property
-    def body(self) -> str:
-        """Contents of the issue comment."""
-        return self.comment["body"]
-
-    @property
-    def id(self) -> int:
-        """Unique identifier of the comment."""
-        return self.comment["id"]
-
-    @property
-    def commenter_username(self) -> str:
-        """Commenter username."""
-        return self.comment["user"]["login"]
-
-    @property
-    def is_pull_comment(self) -> bool:
+    def is_on_pull(self) -> bool:
         """Whether the comment is on a pull request (True) or an issue (False)."""
-        return bool(self.comment.get("pull_request"))
+        return bool(self.issue.pull_request)
+
+    @property
+    def changes(self) -> IssueCommentEditedChangesObject | None:
+        """The changes to the comment if the action was 'edited'."""
+        if self.action == ActionType.EDITED:
+            return IssueCommentEditedChangesObject(self._payload["changes"])
+        return
