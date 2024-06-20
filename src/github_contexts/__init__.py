a5cd36ab4650835c05217c6d846c295ae2fb6fd9
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-06-20 20:05:19 +0200

    add imports

--- src/github_contexts/__init__.py
+++ src/github_contexts/__init__.py
@@ -1,5 +1,2 @@
-from github_contexts.github.context import GitHubContext
-
-
-def context_github(context: dict):
-    return GitHubContext(context=context)
+from github_contexts import github
+from github_contexts.github import GitHubContext
