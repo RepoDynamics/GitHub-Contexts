faf72d792b5a8f9d5120505aae4f08e3d0cb5e76
AAriam <80158628+AAriam@users.noreply.github.com>
2024-11-04 21:07:59 +0100

    Update milestone.py

--- src/github_contexts/github/payload/object/milestone.py
+++ src/github_contexts/github/payload/object/milestone.py
@@ -4,8 +4,10 @@
 from github_contexts.github.payload.object.user import User
 from github_contexts.github.enum import State
 
+from github_contexts.property_dict import PropertyDict as _PropertyDict
 
-class Milestone:
+
+class Milestone(_PropertyDict):
     """GitHub Milestone Object"""
 
     def __init__(self, data: dict):
@@ -15,69 +17,69 @@ class Milestone:
         data : dict
             The `milestone` dictionary contained in the payload.
         """
-        self._milestone = data
+        super().__init__(data)
         return
 
     @property
     def closed_at(self) -> str | None:
-        return self._milestone["closed_at"]
+        return self._data["closed_at"]
 
     @property
     def closed_issues(self) -> int:
-        return self._milestone["closed_issues"]
+        return self._data["closed_issues"]
 
     @property
     def created_at(self) -> str:
-        return self._milestone["created_at"]
+        return self._data["created_at"]
 
     @property
     def creator(self) -> User | None:
-        return User(self._milestone["creator"]) if self._milestone.get("creator") else None
+        return User(self._data["creator"]) if self._data.get("creator") else None
 
     @property
     def description(self) -> str | None:
-        return self._milestone["description"]
+        return self._data["description"]
 
     @property
     def due_on(self) -> str | None:
-        return self._milestone["due_on"]
+        return self._data["due_on"]
 
     @property
     def html_url(self) -> str:
-        return self._milestone["html_url"]
+        return self._data["html_url"]
 
     @property
     def id(self) -> int:
-        return self._milestone["id"]
+        return self._data["id"]
 
     @property
     def labels_url(self) -> str:
-        return self._milestone["labels_url"]
+        return self._data["labels_url"]
 
     @property
     def node_id(self) -> str:
-        return self._milestone["node_id"]
+        return self._data["node_id"]
 
     @property
     def number(self) -> int:
-        return self._milestone["number"]
+        return self._data["number"]
 
     @property
     def open_issues(self) -> int:
-        return self._milestone["open_issues"]
+        return self._data["open_issues"]
 
     @property
     def state(self) -> State:
-        return State(self._milestone["state"])
+        return State(self._data["state"])
 
     @property
     def title(self) -> str:
-        return self._milestone["title"]
+        return self._data["title"]
 
     @property
     def updated_at(self) -> str:
-        return self._milestone["updated_at"]
+        return self._data["updated_at"]
 
     @property
     def url(self) -> str:
-        return self._milestone["url"]
+        return self._data["url"]
