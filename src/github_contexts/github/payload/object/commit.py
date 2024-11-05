1a6b0beda4d8898fc877fcb558358b795bdd68d9
AAriam <80158628+AAriam@users.noreply.github.com>
2024-11-05 05:34:11 +0100

    Update commit.py

--- src/github_contexts/github/payload/object/commit.py
+++ src/github_contexts/github/payload/object/commit.py
@@ -1,60 +1,61 @@
+from github_contexts.property_dict import PropertyDict as _PropertyDict
 from github_contexts.github.payload.object.commit_author import CommitAuthor
 
 
-class Commit:
+class Commit(_PropertyDict):
 
     def __init__(self, commit: dict):
-        self._commit = commit
+        super().__init__(commit)
         return
 
     @property
     def added(self) -> list[str]:
         """Paths of added files."""
-        return self._commit.get("added", [])
+        return self._data.get("added", [])
 
     @property
     def author(self) -> CommitAuthor:
         """Git author information."""
-        return CommitAuthor(self._commit["author"])
+        return CommitAuthor(self._data["author"])
 
     @property
     def committer(self) -> CommitAuthor:
         """Git committer information."""
-        return CommitAuthor(self._commit["committer"])
+        return CommitAuthor(self._data["committer"])
 
     @property
     def distinct(self) -> bool:
         """Whether this commit is distinct from any that have been pushed before."""
-        return self._commit["distinct"]
+        return self._data["distinct"]
 
     @property
     def id(self) -> str:
-        return self._commit["id"]
+        return self._data["id"]
 
     @property
     def message(self) -> str:
-        return self._commit["message"]
+        return self._data["message"]
 
     @property
     def modified(self) -> list[str]:
         """Paths of modified files."""
-        return self._commit.get("modified", [])
+        return self._data.get("modified", [])
 
     @property
     def removed(self) -> list[str]:
         """Paths of removed files."""
-        return self._commit.get("removed", [])
+        return self._data.get("removed", [])
 
     @property
     def timestamp(self) -> str:
         """ISO 8601 (YYYY-MM-DDTHH:MM:SSZ) timestamp of the commit."""
-        return self._commit["timestamp"]
+        return self._data["timestamp"]
 
     @property
     def tree_id(self) -> str:
-        return self._commit["tree_id"]
+        return self._data["tree_id"]
 
     @property
     def url(self) -> str:
         """URL that points to the commit API resource."""
-        return self._commit["url"]
+        return self._data["url"]
