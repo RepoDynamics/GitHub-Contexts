ff9eb681e169abb675b76754bf88fb3a9f725107
AAriam <80158628+AAriam@users.noreply.github.com>
2024-11-05 05:34:06 +0100

    Update changes.py

--- src/github_contexts/github/payload/object/changes.py
+++ src/github_contexts/github/payload/object/changes.py
@@ -1,84 +1,85 @@
+from github_contexts.property_dict import PropertyDict as _PropertyDict
 from github_contexts.github.payload.object.issue import Issue
 from github_contexts.github.payload.object.repository import Repository
 
 
-class IssueOpenedChanges:
+class IssueOpenedChanges(_PropertyDict):
 
     def __init__(self, changes: dict):
-        self._changes = changes
+        super().__init__(changes)
         return
 
     @property
     def old_issue(self) -> Issue | None:
-        return Issue(self._changes["old_issue"]) if self._changes.get("old_issue") else None
+        return Issue(self._data["old_issue"]) if self._data.get("old_issue") else None
 
     @property
     def old_repository(self) -> Repository:
-        return Repository(self._changes["old_repository"])
+        return Repository(self._data["old_repository"])
 
 
-class IssueTransferredChanges:
+class IssueTransferredChanges(_PropertyDict):
 
     def __init__(self, changes: dict):
-        self._changes = changes
+        super().__init__(changes)
         return
 
     @property
     def new_issue(self) -> Issue:
-        return Issue(self._changes["new_issue"])
+        return Issue(self._data["new_issue"])
 
     @property
     def new_repository(self) -> Repository:
-        return Repository(self._changes["new_repository"])
+        return Repository(self._data["new_repository"])
 
 
-class IssueEditedChanges:
+class IssueEditedChanges(_PropertyDict):
 
     def __init__(self, changes: dict):
-        self._changes = changes
+        super().__init__(changes)
         return
 
     @property
     def body(self) -> dict | None:
-        return self._changes.get("body")
+        return self._data.get("body")
 
     @property
     def title(self) -> dict | None:
-        return self._changes.get("title")
+        return self._data.get("title")
 
 
-class PullRequestEditedChanges:
+class PullRequestEditedChanges(_PropertyDict):
 
     def __init__(self, changes: dict):
-        self._changes = changes
+        super().__init__(changes)
         return
 
     @property
     def base_ref(self) -> str | None:
-        return self._changes.get("base", {}).get("ref", {}).get("from")
+        return self._data.get("base", {}).get("ref", {}).get("from")
 
     @property
     def base_sha(self) -> str | None:
-        return self._changes.get("base", {}).get("sha", {}).get("from")
+        return self._data.get("base", {}).get("sha", {}).get("from")
 
     @property
     def body(self) -> str | None:
         """"The previous version of the body."""
-        return self._changes.get("body", {}).get("from")
+        return self._data.get("body", {}).get("from")
 
     @property
     def title(self) -> dict | None:
         """The previous version of the title."""
-        return self._changes.get("title", {}).get("from")
+        return self._data.get("title", {}).get("from")
 
 
-class IssueCommentEditedChanges:
+class IssueCommentEditedChanges(_PropertyDict):
 
     def __init__(self, changes: dict):
-        self._changes = changes
+        super().__init__(changes)
         return
 
     @property
     def body(self) -> str | None:
         """The previous version of the body."""
-        return self._changes.get("body", {}).get("from")
+        return self._data.get("body", {}).get("from")
