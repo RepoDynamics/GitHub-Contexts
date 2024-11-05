4197f2871721cd6c09158bb32f82b55c4142ea7f
AAriam <80158628+AAriam@users.noreply.github.com>
2024-11-05 05:34:13 +0100

    Update commit_author.py

--- src/github_contexts/github/payload/object/commit_author.py
+++ src/github_contexts/github/payload/object/commit_author.py
@@ -1,23 +1,25 @@
+from github_contexts.property_dict import PropertyDict as _PropertyDict
 
-class CommitAuthor:
+
+class CommitAuthor(_PropertyDict):
 
     def __init__(self, author: dict):
-        self._author = author
+        super().__init__(author)
         return
 
     @property
     def date(self) -> str | None:
-        return self._author.get("date")
+        return self._data.get("date")
 
     @property
     def email(self) -> str | None:
-        return self._author.get("email")
+        return self._data.get("email")
 
     @property
     def name(self) -> str | None:
         """The Git author's name."""
-        return self._author.get("name")
+        return self._data.get("name")
 
     @property
     def username(self) -> str | None:
-        return self._author.get("username")
+        return self._data.get("username")
